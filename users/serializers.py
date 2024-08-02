71a27001629f3cbb6d476b490a99f576e8c95b42
danil klimenko <sos.danil@yandex.ru>
2024-08-02 18:17:50 +0500

    Реализовал CRUD на модель Пользователя

---
+++ users/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+
+from users.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
+class UserCreateUpdateSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('email', 'first_name', 'last_name', 'phone', 'image',)
--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,14 @@
+from django.urls import path
+
 from users.apps import UsersConfig
+from users.views import UserCreateAPIView, UserListAPIView, UserRetrieveAPIView, UserUpdateAPIView, UserDestroyAPIView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
-
+    path('register/', UserCreateAPIView.as_view(), name='users_register'),
+    path('list/', UserListAPIView.as_view(), name='users_list'),
+    path('retrieve/<int:pk>/', UserRetrieveAPIView.as_view(), name='users_retrieve'),
+    path('update/<int:pk>/', UserUpdateAPIView.as_view(), name='users_update'),
+    path('delete/<int:pk>/', UserDestroyAPIView.as_view(), name='users_delete'),
 ]
